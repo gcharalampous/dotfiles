9cf0614d36bcd345ad7db92741d6979d200086aa
Georgios Charalampous <gchara14@gmail.com>
2026-02-19 19:14:53 -0800

    Add Fedora auto-install script

---
+++ run_once_install_fedora.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# =============================================================================
+# OH MY ZSH
+# =============================================================================
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# =============================================================================
+# ZSH PLUGINS
+# =============================================================================
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-vi-mode" ]; then
+    git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+fi
+
+# =============================================================================
+# POWERLEVEL10K
+# =============================================================================
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+fi
+
+# =============================================================================
+# TOOLS (DNF)
+# =============================================================================
+sudo dnf install -y fzf bat lsd btop tldr
